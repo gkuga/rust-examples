810b670501db0ae61ca2351ccde19a5769d853b3
Gaku Yasui <isuya.kuga@gmail.com>
2020-02-13 07:05:48 +0000

    Add maybeuninit

---
+++ maybeuninit/src/main.rs
@@ -0,0 +1,14 @@
+use std::mem::MaybeUninit;
+
+unsafe fn make_vec(out: *mut Vec<i32>) {
+    out.write(vec![1, 2, 3]);
+}
+
+fn main() {
+    let mut v = MaybeUninit::uninit();
+    unsafe {
+        make_vec(v.as_mut_ptr());
+    }
+    let v = unsafe { v.assume_init() };
+    println!("{:?}", v);
+}
