638e9905897a5808ed2b0f02fec731e2ac8c6364
gkuga <vagrant@localhost.localdomain>
2020-03-21 20:22:07 +0000

    Added manually_drop

--- manually_drop/src/main.rs
+++ manually_drop/src/main.rs
@@ -1,9 +1,12 @@
+#![allow(unused)]
+
 use std::mem::ManuallyDrop;
 
 fn main() {
     let mut orig_val = ManuallyDrop::new("something".to_string());
+    println!("{:?}", orig_val);
     unsafe {
-        let mut taked_val = ManuallyDrop::take(&mut orig_val);
+        let taked_val = ManuallyDrop::take(&mut orig_val);
     }
     println!("{:?}", orig_val);
 }
