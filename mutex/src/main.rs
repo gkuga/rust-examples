9d96bc74ec9e61c4dcd65c752dcf477521772e7f
gkuga <gaku.yasui@gmail.com>
2022-10-28 14:07:11 +0900

    Update async

--- mutex/src/main.rs
+++ mutex/src/main.rs
@@ -1,30 +1,11 @@
 use std::sync::Mutex;
 use std::thread;
 
-struct CanIncrement {
-    mutex: Mutex<i32>,
-}
-impl CanIncrement {
-    fn increment(&self) {
-        let mut lock = self.mutex.lock().unwrap();
-        *lock += 1;
-    }
-}
-
-async fn do_something_async() {
-    println!("do_something_async");
-}
-
-async fn increment_and_do_stuff(can_incr: &CanIncrement) {
-    can_incr.increment();
-    do_something_async().await;
-}
-
 fn main() {
-    let counter = std::RC.new(Mutex::new(0));
+    let counter = std::sync::Arc::new(Mutex::new(0));
     let mut handles = vec![];
-
     for _ in 0..10 {
+        let counter = std::sync::Arc::clone(&counter);
         let handle = thread::spawn(move || {
             let mut num = counter.lock().unwrap();
 
